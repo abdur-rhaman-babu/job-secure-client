f9d50255f2c581f47fbb1ba1bf9636e24dd1b98e
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-27 12:01:54 +0600

    validation bid collection

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -3,11 +3,11 @@
 import { Link } from 'react-router-dom'
 import { format } from "date-fns";
 const JobCard = ({job}) => {
-  const {price, title, description, category, deadLine, bid_count} = job || {}
+  const {price, title, description, category, deadLine, bid_count, _id} = job || {}
 
   return (
     <Link
-      to={`/job/1`}
+      to={`/job/${_id}`}
       className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'
     >
       <div className='flex items-center justify-between'>
@@ -31,7 +31,7 @@ const JobCard = ({job}) => {
           {description}
         </p>
         <p className='mt-2 text-sm font-bold text-gray-600 '>
-          Range: {`${price.min_price} - ${price.max_price}`}
+          Range: {`$${price.minPrice} - $${price.maxPrice}`}
         </p>
         <p className='mt-2 text-sm font-bold text-gray-600 '>Total Bids: {bid_count}</p>
       </div>
